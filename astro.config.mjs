8e52fc8605dc58aefbb710146e7af77f7e16b731
theAdhocracy <murrayadcock@gmail.com>
2024-08-29 15:51:22 +0100

    :wrench: Updating config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
-import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
+
+let adapter = netlify();
+
+// Use node adapter when running local builds (cannot run Netlify locally)
+if (process.argv[3] === "--node" || process.argv[4] === "--node") {
+	adapter = node({ mode: "standalone" });
+}
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		css: {
+			transformer: "lightningcss",
+		},
+	},
+	output: "server",
+	adapter: adapter,
+	base: "/journal",
+});
