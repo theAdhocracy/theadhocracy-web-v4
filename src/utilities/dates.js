38f0db23302a7941abbf1f690b3bc7b6d94fdcd6
theAdhocracy <murrayadcock@gmail.com>
2024-09-02 16:52:46 +0100

    :recycle: Clone pages from Gatsby

---
+++ src/utilities/dates.js
@@ -0,0 +1,8 @@
+// Format Date: DD MMM YYYY
+export const formatDate = (value) => {
+	let date = new Date(value);
+	const day = date.toLocaleString("default", { day: "2-digit" });
+	const month = date.toLocaleString("default", { month: "short" });
+	const year = date.toLocaleString("default", { year: "numeric" });
+	return day + " " + month + " " + year;
+};
