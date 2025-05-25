d85700079dc32a8f3092a411b2ef6422cc7a1742
theAdhocracy <murrayadcock@gmail.com>
2025-05-26 00:11:44 +0100

    :fire: Remove dev changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
 	},
 	output: "hybrid",
 	adapter: node({ mode: "standalone" }),
-	site: "https://v4.theadhocracy.co.uk",
+	site: "https://theadhocracy.co.uk",
 	integrations: [sitemap()],
 	trailingSlash: "never",
 	build: {
