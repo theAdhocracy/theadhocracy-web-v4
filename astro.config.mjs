583c1262c8d13c0f1fe458749220800d43924be4
theAdhocracy <murrayadcock@gmail.com>
2025-05-25 12:49:31 +0100

    :fire: Remove Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
-import netlify from "@astrojs/netlify";
 import node from "@astrojs/node";
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
-
-let adapter = netlify();
-let dirFormat = "file";
-
-// Use node adapter when running local builds (cannot run Netlify locally)
-if (process.argv[3] === "--node" || process.argv[4] === "--node") {
-	adapter = node({ mode: "standalone" });
-	dirFormat = "directory"; // controls trailing slashes via dir output
-}
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,11 +11,11 @@ export default defineConfig({
 		},
 	},
 	output: "hybrid",
-	adapter: adapter,
-	site: "https://theadhocracy.co.uk",
+	adapter: node({ mode: "standalone" }),
+	site: "https://v4.theadhocracy.co.uk",
 	integrations: [sitemap()],
 	trailingSlash: "never",
 	build: {
-		format: dirFormat,
+		format: "directory",
 	},
 });
